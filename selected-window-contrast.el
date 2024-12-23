05c4832b899e2d6e4f33e1e687b7bba593b23fc3
none <none>
2024-12-23 15:28:17 +0000

    fix melpazoid warnings2

--- selected-window-contrast.el
+++ selected-window-contrast.el
@@ -1,4 +1,4 @@
-;;; selected-window-contrast.el --- Set relative brightness of text-background for window and modeline   -*- lexical-binding: t -*-
+;;; selected-window-contrast.el --- Relative brightness for text-background in window and modeline   -*- lexical-binding: t -*-
 
 ;; Copyright (c) 2024 github.com/Anoncheg1,codeberg.org/Anoncheg
 
@@ -25,7 +25,6 @@
 ;; see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
 
 ;;; Commentary:
-
 ;; Loop windows at frame, measure and adjust contrast.  Allow to set
 ;;  color (face) of background and text by comparing their
 ;;  brightness.
@@ -36,22 +35,25 @@
 ;; Usage:
 
 ;;  (require 'selected-window-contrast)
-;;  ;; - increase contrast for selected window
-;;  (setopt selected-window-contrast-selected-magnitude-text 0.8)  ; default = 1.0
-;;  (setopt selected-window-contrast-selected-magnitude-background 0.9)  ; default = 1.0
-;;  ;; - decrease conrtrast for other windows
-;;  (setopt selected-window-contrast-not-sel-magnitude-text 1.1)  ; default = 1.0
-;;  (setopt selected-window-contrast-not-sel-magnitude-background 1.1)  ; default = 1.0
-;;  (add-hook 'buffer-list-update-hook #'selected-window-contrast-highlight-selected-window)
+;;  ;; - increase contrast for selected window (default = 1.0)
+;;  (setopt selected-window-contrast-selected-magnitude-text 0.8)
+;;  (setopt selected-window-contrast-selected-magnitude-background 0.9)
+;;  ;; - decrease conrtrast for other windows (default = 1.0)
+;;  (setopt selected-window-contrast-not-sel-magnitude-text 1.1)
+;;  (setopt selected-window-contrast-not-sel-magnitude-background 1.1)
+;;  (add-hook 'buffer-list-update-hook
+;;           #'selected-window-contrast-highlight-selected-window)
 ;;  ;; - for case of call: $ emacsclient -c ~/file
-;;  (add-hook 'server-after-make-frame-hook #'selected-window-contrast-highlight-selected-window)
+;;  (add-hook 'server-after-make-frame-hook
+;;           #'selected-window-contrast-highlight-selected-window)
 
 ;; To increase contrast of selected modeline:
 
-;;  (selected-window-contrast-change-modeline 0.7 0.7) ; increase contrast
+;;  (selected-window-contrast-change-modeline 0.7 0.7)
 
 ;; How this works:
-;;  1) We get color with ```face-attribute (selected-frame)``` for foreground and backgraound.
+;;  1) We get color with `face-attribute' `selected-frame' for
+;;  foreground and backgraound.
 ;;  2) Convert color to HSL
 ;;  3) adjust brightness in direction of foreground-background average
 ;;  4) convert color to RGB, then to HEX
